7c017d0ac1cbf497ccdb9767d93bd2ba7a8cc362
roman_lubushkin <ra.lubushkin@gmail.com>
2017-02-18 16:12:24 +0800

    move to net

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var uuid = require('uuid');
+var cljs = require('collaborativejs');
 
 // region ---- start server
 console.log('Waiting for the server to startup');
@@ -27,10 +28,12 @@ process.on('exit', function() {
 
 
 // testing params
-var totalItemsCount = 400000;
+var totalItemsCount = 100000;
 var itemsPerRequest = 200;
-var sendingInterval = 1;
+var generatingInterval = 4;
+var sendingInterval = 2;
 var statusUpdateInterval = 500;
+var timerObj = {setTimeout: setTimeout, clearTimeout: clearTimeout};
 
 // testing results
 var testPassed = false;
@@ -43,160 +46,195 @@ var requestsFailed = 0;
 var requestsSucceed = 0;
 
 // items stat
-var itemsSent = 0;
-var itemsDelivered = 0;
-var itemsLost = 0;
-var itemsResent = 0;
+var opsGenerated = 0;
+var opsSent = 0;
+var opsDelivered = 0;
+var opsLost = 0;
+var opsResent = 0;
 
-var resendingQueue = [];
-var returnedItems = [];
-var returnedItemsMap = {};
+var returnedOps = [];
+var returnedOpsMap = {};
 
 // server stat
 var requestReceived = NaN;
-var serverItemsReceived = NaN;
-var serverItemsStored = NaN;
-var serverKeysStored = NaN;
-var serverUpdatesSent = NaN;
+var serverOpsReceived = NaN;
+var serverOpsStored = NaN;
+var serverIdsStored = NaN;
+var serverOpsSent = NaN;
+
+// document
+var document = null;
+
 
 
 function runTests() {
-  var sendingIntervalId = setInterval(function() {
-    if (itemsSent < totalItemsCount) {
-      commitItems();
+  var options = {
+    method: 'post',
+    json: true,
+    url: 'http://localhost:3000/create'
+  };
+
+  request(options, function(error, response, body) {
+    document = body.document;
+    startSendingOps();
+  });
+}
+
+
+function startSendingOps() {
+  var net = new cljs.net.Http(returnedOps.length, timerObj);
+  net.requestAbortAllowed(false);
+  net.sendFn(sendFunction);
+
+
+  var generatingIntervalId = setInterval(function() {
+    if (opsGenerated < totalItemsCount) {
+      var ops = generateOps();
+      opsGenerated += ops.length;
+      net.send(ops);
     } else {
-      clearInterval(sendingIntervalId);
+      clearInterval(generatingIntervalId);
     }
-  }, sendingInterval);
+  }, generatingInterval);
 
   var statusUpdateIntervalId = setInterval(function() {
     if (isTestComplete()) {
       clearInterval(statusUpdateIntervalId);
+      net.stop();
       exit();
     } else {
       requestStatus();
     }
   }, statusUpdateInterval);
+
+  net.start(sendingInterval);
 }
 
 
-function commitItems() {
-  var updates = generateItems();
+function sendFunction(ops, packageIndex, onComplete) {
   var options = {
     method: 'post',
-    body: {updates: updates, packageIndex: returnedItems.length},
+    body: {
+      documentId: document.id,
+      packageIndex: packageIndex,
+      ops: ops
+    },
     json: true,
     url: 'http://localhost:3000/commit'
   };
-
-  itemsSent += updates.length;
+  opsSent += ops.length;
   requestsSent++;
-
-  var callback = onCommitComplete.bind(this, updates);
+  var callback = onCommitComplete.bind(this, ops, onComplete);
   request(options, callback);
+
+  return function() {
+  };
 }
 
-function onCommitComplete(items, error, response, body) {
+function onCommitComplete(ops, cotOnCompleteCallback, error, response, body) {
+  //deliveredUpdates, isSuccess, isAbort, packageIndex, receivedUpdates
   if (error) {
     requestsFailed++;
-    itemsLost += items.length;
+    opsLost += ops.length;
     netErrors += '\n' + error.toString();
-    resendingQueue = resendingQueue.concat(items);
+
+    cotOnCompleteCallback(false, false, returnedOps.length, body.ops);
   } else {
     requestsSucceed++;
-    itemsDelivered += items.length;
+    opsDelivered += ops.length;
+    receiveOps(body.ops);
 
-    receiveItems(body.updates);
+    cotOnCompleteCallback(true, false, returnedOps.length, body.ops);
   }
   requestsComplete++;
 }
 
-function generateItems() {
+function generateOps() {
   var result = [];
 
   for (var i = 0; i < itemsPerRequest; i++) {
-    result.push({uuid: uuid.v4()});
+    result.push({id: uuid.v4()});
   }
-
-  itemsResent += resendingQueue.length;
-  result = result.concat(resendingQueue.splice(0));
 
   return result;
 }
 
-function receiveItems(updates) {
-  for (var i = 0, count = updates.length; i < count; i++) {
-    var pack = updates[i];
-    if (!returnedItemsMap[pack.uuid]) {
-      returnedItemsMap[pack.uuid] = pack;
-      returnedItems.push(pack);
+function receiveOps(ops) {
+  for (var i = 0, count = ops.length; i < count; i++) {
+    var op = ops[i];
+    if (!returnedOpsMap[op.id]) {
+      returnedOpsMap[op.id] = ops;
+      returnedOps.push(ops);
     }
   }
 }
 
 function isTestComplete() {
-  var allItemsSent = totalItemsCount == itemsSent;
-  var allItemsDeliveredToServer = totalItemsCount == serverItemsReceived;
+  var allOpsSent = totalItemsCount == opsSent;
+  var allOpsDeliveredToServer = totalItemsCount == serverOpsReceived;
   var allRequestsComplete = requestsSent == requestsComplete;
-  return allItemsSent && allItemsDeliveredToServer && allRequestsComplete;
+  return allOpsSent && allOpsDeliveredToServer && allRequestsComplete;
 }
 
 
-function requestStatus() {
+function requestStatus(opt_callback) {
   var options = {
     method: 'post',
     json: true,
-    url: 'http://localhost:3000/status'
+    body: {documentId: document.id},
+    url: 'http://localhost:3000/stat'
   };
 
   request(options, function(error, response, body) {
     requestReceived = body.requestReceived;
-    serverItemsReceived = body.updatesReceived;
-    serverItemsStored = body.updatesStored;
-    serverKeysStored = body.uuidsStored;
-    serverUpdatesSent = body.updatesSent;
+    serverOpsReceived = body.opsReceived;
+    serverOpsSent = body.opsSent;
+
+    serverOpsStored = body.opsStored;
+    serverIdsStored = body.idsStored;
 
     checkTestPassed();
     reportStatus();
+
+    if (opt_callback) opt_callback();
   });
 }
 
 function checkTestPassed() {
   testPassed =
-        totalItemsCount == serverItemsStored &&
-        totalItemsCount == serverKeysStored &&
-        totalItemsCount == returnedItems.length;
+      totalItemsCount == serverOpsStored &&
+      totalItemsCount == serverIdsStored &&
+      totalItemsCount == returnedOps.length;
 }
 
 
 function reportStatus() {
   console.log(
       'Status report' +
-      '\n    Requests - sent: %d, received: %d, complete: %d' +
-      '\n    Items - sent: %d, delivered: %d, lost: %d, resent: %d, returned: %d' +
-      '\n    Server - received: %d, stored: %d, keys stored: %d, sent: %d' +
+      '\n    Requests - sent: %d, received by server: %d, complete: %d, succeed: %d, failed: %d' +
+      '\n    Client Ops - sent: %d, delivered: %d, lost: %d, resent: %d, returned: %d' +
+      '\n    Server Ops - received: %d, stored: %d, keys stored: %d, sent: %d' +
       '\nTest complete: %s',
-      requestsSent, requestReceived, requestsComplete,
-      itemsSent, itemsDelivered, itemsLost, itemsResent, returnedItems.length,
-      serverItemsReceived, serverItemsStored, serverKeysStored, serverUpdatesSent,
+      requestsSent, requestReceived, requestsComplete, requestsSucceed, requestsFailed,
+      opsSent, opsDelivered, opsLost, opsResent, returnedOps.length,
+      serverOpsReceived, serverOpsStored, serverIdsStored, serverOpsSent,
       isTestComplete()
   );
 }
 
 
 function exit() {
-  checkTestPassed();
-  reportStatus();
+  setTimeout(function() {
+    requestStatus(function() {
+      if (netErrors) console.log('Net errors:\n' + netErrors);
 
-  if (netErrors) console.log('Net errors:\n' + netErrors);
-
-  if (testPassed) {
-    console.log('Test passed');
-    process.exit(0);
-  } else {
-    console.error('Test failed');
-    process.exit(1);
-  }
-
-
+      if (testPassed) {
+        console.log('Test passed');
+        process.exit(0);
+      } else {
+        console.error('Test failed');
+        process.exit(1);
+      }
+    });
+  }, 1000);
 }
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var uuid = require('uuid');
 
 // memory storage
-var storage = [];
-var storageMap = {};
+var storage = {};
 
 // stat
 var requestReceived = 0;
-var updatesReceived = 0;
-var updatesSent = 0;
+var opsReceived = 0;
+var opsSent = 0;
 
 // run node server
 var app = express();
@@ -19,53 +19,95 @@ app.get('/', function(req, res) {
   res.send('ok');
 });
 
-app.post('/commit', function(req, res) {
-  var packageIndex = req.body.packageIndex;
-  var updates = req.body.updates;
 
-  requestReceived++;
-  updatesReceived += updates.length;
-
-  applyUpdates(updates);
-
-  var returnUpdates = storage.slice(packageIndex, storage.length);
-  updatesSent += returnUpdates.length;
-
-  var result  = {
-    updates: returnUpdates
+app.post('/create', function(req, res) {
+  var document = {
+    id: uuid.v4(),
+    data: 'Hello World',
+    ops: [],
+    opsMap: {},
+    context: null
   };
 
-  res.status(200);
-  res.send(JSON.stringify(result));
+  saveDocument(document, function() {
+    res.status(200);
+    res.send(JSON.stringify({document: document}));
+  });
+});
+
+app.post('/commit', function(req, res) {
+  var documentId = req.body.documentId;
+  var packageIndex = req.body.packageIndex;
+  var ops = req.body.ops;
+
+  requestReceived++;
+  opsReceived += ops.length;
+
+  getDocument(documentId, function(document) {
+    applyOps(document, ops);
+
+    var returnOps = searchForOps(document, packageIndex);
+    opsSent += returnOps.length;
+
+    var result = {ops: returnOps};
+    res.status(200);
+    res.send(JSON.stringify(result));
+  });
 });
 
 
-function applyUpdates(updates) {
-  for (var i = 0, count = updates.length; i < count; i++) {
-    var pack = updates[i];
-    if (!storageMap[pack.uuid]) {
-      storageMap[pack.uuid] = pack;
-      storage.push(pack);
+
+app.post('/stat', function(req, res) {
+  var documentId = req.body.documentId;
+  getDocument(documentId, function(document) {
+    var status = {
+      // server stat
+      requestReceived: requestReceived,
+      opsReceived: opsReceived,
+      opsSent: opsSent,
+
+      // document stat
+      opsStored: document.ops.length,
+      idsStored: Object.keys(document.opsMap).length
+    };
+    res.status(200);
+    res.send(JSON.stringify(status));
+  });
+});
+
+// region ---- working with document ops
+function applyOps(document, ops) {
+  for (var i = 0, count = ops.length; i < count; i++) {
+    var op = ops[i];
+    if (!document.opsMap[op.id]) {
+      document.opsMap[op.id] = op;
+      document.ops.push(op);
     }
   }
 }
 
-function searchForUpdates(packageIndex) {
-  //return
+function searchForOps(document, packageIndex) {
+  return document.ops.slice(packageIndex, document.ops.length);
+}
+// endregion
+
+
+// region ---- working with in-memory DB (rework it with any DB you like)
+function saveDocument(document, callback) {
+  storage[document.id] = document;
+  callback();
 }
 
 
-app.post('/status', function(req, res) {
-  var status = {
-    requestReceived: requestReceived,
-    updatesReceived: updatesReceived,
-    updatesStored: storage.length,
-    uuidsStored: Object.keys(storageMap).length,
-    updatesSent: updatesSent
-  };
-  res.status(200);
-  res.send(JSON.stringify(status));
-});
+function updateDocument(document, callback) {
+  storage[document.id] = document;
+  callback();
+}
+
+function getDocument(id, callback) {
+  callback(storage[id]);
+}
+// endregion
 
 
 app.listen(3000, function() {
